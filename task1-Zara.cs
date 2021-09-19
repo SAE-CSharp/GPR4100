e75963e2013796c8e4cfed7bc595cee3d7f92d85
SAE-CSharp <90071512+SAE-CSharp@users.noreply.github.com>
2021-09-19 14:41:59 +0100

    Update and rename task1-Zara to task1-Zara.cs

---
+++ task1-Zara.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace First
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string studentName; // declare a variable
+
+            studentName = "Tom"; // assign value to the variable
+
+
+            string student = "Josh";
+
+            Console.WriteLine("Hello "+studentName);
+            Console.WriteLine("Hello "+student);
+
+            Console.WriteLine("************************************************************************************");
+
+            int v1, v2, sum;
+
+                v1 = 20;
+                v2 = 30;
+            sum = v1 + v2;
+
+            Console.WriteLine("the sum is: "+sum);
+
+
+           
+        }
+    }
+}
+
