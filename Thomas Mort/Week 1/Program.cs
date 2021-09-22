4af523cd677ba46eec51c8ac2bbd6f19cecbdced
ThomasMORTsae <90903480+ThomasMORTsae@users.noreply.github.com>
2021-09-22 16:51:54 +0100

    Operators

--- Thomas Mort/Week 1/Program.cs
+++ Thomas Mort/Week 1/Program.cs
@@ -1,22 +1,56 @@
 ﻿using System;
 
-namespace Project_1
+namespace OperatorsV2
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) //main is the START POINT of the program.
         {
-            int Age;
-            Age = 18;
+            int result;
+            int x = 50, y = 10;
+            result = (x + y);
+            Console.WriteLine("Addition Operator:" + result);
+            result = (x - y);
+            Console.WriteLine("Subtraction Operator:" + result);
+            result = (x * y);
+            Console.WriteLine("Multiplication Operator:" + result);
+            result = (x / y);
+            Console.WriteLine("Division Operator:" + result);
+            result = (x % y);
+            Console.WriteLine("Modulo Operator:" + result); //remainder of the division
 
-            float Distance;
-            Distance = 100.5f;
+            Console.WriteLine("***************************************************");
+            Console.WriteLine("***************************************************");
 
-            char ch = 'T';
+            int a = 17;
+            int z = 13;
 
-            string FullSentence;
-            FullSentence = ("Tom is\t"+ Age +"\nAnd he has walked\t"+ Distance +" miles today.\n His name begins with "+ch);
-            Console.WriteLine(FullSentence);
+            Console.WriteLine("The result of int/int is:" + a / z);
+            Console.WriteLine("The result of float/int is:" + (float)a / z);
+            Console.WriteLine("The result of int/float is:" + a / (float)z);
+            Console.WriteLine("The result of float/float is:" + (float)a / (float)z);
+
+            Console.WriteLine("***************************************************");
+            Console.WriteLine("***************************************************");
+            int b = 5;
+            b++; //a=6
+            int c = 4;
+            c--; //b=3
+            Console.WriteLine("b++ equals:" +b);
+            Console.WriteLine("c-- equals:" +c);
+
+            Console.WriteLine("***************************************************");
+            Console.WriteLine("***************************************************");
+
+            int d = 10;
+            d += 2; // d=d+2 similar to each other but shorter meaning more time to spend on other things.
+            d -= 2; // d=d-2 
+            d *= 2; // d=d*2
+            d /= 2; // d=d/2
+
+
+            Console.WriteLine("Press Enter Key to Exit..");
+            Console.ReadLine();
         }
     }
 }
