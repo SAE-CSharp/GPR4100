acc7af3dc3fde363c9bee8793dc9dcb96893976a
UserTaken01 <90903609+UserTaken01@users.noreply.github.com>
2021-09-22 15:25:14 +0100

    Update HelloCSharp.cs

--- brandon/week1/HelloCSharp.cs
+++ brandon/week1/HelloCSharp.cs
@@ -1,1 +1,23 @@
 //Csharp thing
+using System;
+
+namespace excorcise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name = "Bruh";
+            int Age = 18;
+            float time = 12.5f;
+            float time2 = 07.00f;
+            char f = 'f';
+            char i = 'i';
+            char n = 'n';
+            Console.WriteLine("Hello my name is "+Name+"\n I\'m "+Age+"\n The time is probably not \t"+time);
+            Console.WriteLine("who are you?");
+            string peopleName = Console.ReadLine();
+            Console.WriteLine("Hello "+peopleName+"\n\t\twhy did i wake up at "+time2+"\t\t\t\teverything hurts");
+        }
+    }
+}
