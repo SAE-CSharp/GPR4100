d1964bfd0579871bfbf30af16a248cc68907a7c5
ThomasMORTsae <90903480+ThomasMORTsae@users.noreply.github.com>
2021-09-22 15:24:07 +0100

    Created Program 1 (Story program)

---
+++ Thomas Mort/Week 1/Project 1/Project 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Project_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Age;
+            Age = 18;
+
+            float Distance;
+            Distance = 100.5f;
+
+            char ch = 'T';
+
+            string FullSentence;
+            FullSentence = ("Tom is\t"+ Age +"\nAnd he has walked\t"+ Distance +" miles today.\n His name begins with "+ch);
+            Console.WriteLine(FullSentence);
+        }
+    }
+}
