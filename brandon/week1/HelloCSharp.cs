6f7046e1d08f9ea8399f5e1468b1f6409a9139bb
UserTaken01 <90903609+UserTaken01@users.noreply.github.com>
2021-09-22 16:08:30 +0100

    Update HelloCSharp.cs

--- brandon/week1/HelloCSharp.cs
+++ brandon/week1/HelloCSharp.cs
@@ -19,6 +19,25 @@ namespace excorcise1
             string peopleName = Console.ReadLine();
             Console.WriteLine("Hello "+peopleName+"\n\t\twhy did i wake up at "+time2+"\t\t\t\teverything hurts");
             Console.WriteLine(f+" "+i+" "+n);
+
+            int result;
+            int x = 10;
+            int y = 20;
+            result = (x + y);
+            Console.WriteLine("x + y is " + result);
+            result = (x - y); // result value can be changed without being declared agfain it has already been declared before, just not given a value
+            Console.WriteLine("X minus Y is " + result);
+            result = (x * y);
+            Console.WriteLine("x * y is " + result);
+            result = (x / y);
+            Console.WriteLine("X/Y is "+ result);
+            result = (x % y);
+            Console.WriteLine("X mod Y is " + result);
+            x--; // decrementing the value of x by one, must be done seperately as it is affecting the value of a variable
+            y++; // incrimenting the value of y by one
+            result = (x + y);
+            Console.WriteLine("(x-1) + (y+1) is " + result);
+        
         }
     }
 }
