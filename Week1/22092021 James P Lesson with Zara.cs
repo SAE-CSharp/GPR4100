d2f24ed48672ac052c5ccb7daa066dfaf3a289ed
JayPenter <90903580+JayPenter@users.noreply.github.com>
2021-09-22 15:26:43 +0100

    Add files via upload

---
+++ Week1/22092021 James P Lesson with Zara.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _22092021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int Age = 22;
+            int Time = 30;
+
+            Console.WriteLine("My name is James\n " + "it\'ll take me " + Time + " minutes to get from my home to work\n I get on the Liverpool train\\ Northern line\\ at Orrell Park station");
+        }
+    }
+}
+ 
