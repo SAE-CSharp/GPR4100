1b22aa0f23e866fd093ffc123bf57d87041026b6
Jack-Norris <90903491+Jack-Norris@users.noreply.github.com>
2021-09-22 15:35:56 +0100

    Create HelloC#.cs

---
+++ Jack N/Week1/HelloC#.cs
@@ -0,0 +1,7 @@
+Hello C#. 
+    string name = "Jack";
+    string course = "Games Programming";
+
+
+            Console.WriteLine("My name is "+ name);
+            Console.WriteLine("I\'m studying "+ course);
