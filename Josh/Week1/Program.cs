18f0777d4e414def47d8463cfab408bd4b5a8983
Jfranco2000 <90903522+Jfranco2000@users.noreply.github.com>
2021-09-22 15:28:20 +0100

    Data types

---
+++ Josh/Week1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercise_1__Josh_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            string name;
+            float hieght;
+            char initial;
+
+            age = 21;
+            name = "josh";
+            hieght = 5.11f;
+            initial = 'J';
+
+            Console.WriteLine(initial + "\tHi my name is " + name + " \n\tI\'m " + age + " \n\tand I am " + hieght + " feet tall.");
+
+        }
+    }
+}
