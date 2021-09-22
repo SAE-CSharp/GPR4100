dd39299e0d587b1dc34819553c81858eb55a9069
AaronHille <90903527+AaronHille@users.noreply.github.com>
2021-09-22 15:35:40 +0100

    Update Hello_SheeshHarp.cs

--- Aaron H/Week 1/Hello_SheeshHarp.cs
+++ Aaron H/Week 1/Hello_SheeshHarp.cs
@@ -2,5 +2,5 @@ Hello C#
 int release = 2020;
 string name = "DOOM Eternal";
 float price = 49.99f;
-Console.WriteLine("My name is" + name + "and I'm studying at SAE \"GPR4100\" ");
+Console.WriteLine("My name is " + name + " and I'm studying at SAE \"GPR4100\" ");
 Console.WriteLine("I study using the folder C:\\User\\Admin\\tingzzz");
