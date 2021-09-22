41bd7a0da043b01c66a39d82af7619d501ec0d95
Daisy <90903464+Dai9y@users.noreply.github.com>
2021-09-22 15:26:20 +0100

    Print sentence to console
    Create sentence using int, string, float and char. 
    Print sentence to console.

--- Daisy/Week1/HelloCSharp.cs
+++ Daisy/Week1/HelloCSharp.cs
@@ -1,1 +1,22 @@
 
+using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 24;
+            string name = "Daisy";
+            float drink = 1.5f;
+            char litre = 'L';
+
+            Console.WriteLine("my name is " + name + " I am " + age + " years old. I drink " + drink + " " + litre + " of san pellegrino");
+           
+            /******************************/
+
+            
+        }
+    }
+}
