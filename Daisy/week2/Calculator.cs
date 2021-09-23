ccc53fb4babac1c3d7d59f30a645dc1c4c763f31
Daisy <90903464+Dai9y@users.noreply.github.com>
2021-09-23 17:06:27 +0100

    Calculator application
    Built calculator in C#

---
+++ Daisy/week2/Calculator.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            char opp;
+
+            Console.WriteLine("Enter the num1");
+            num1 = Int32.Parse(Console.ReadLine());
+            opp = char.Parse(Console.ReadLine());
+            num2 = Int32.Parse(Console.ReadLine());
+            
+
+            if (opp == '+')
+            {
+                Console.WriteLine("The answer is " + (num1 + num2));
+            }
+            else if (opp == '-')
+            {
+                Console.WriteLine("The answer is " + (num1 - num2));
+            }
+            else if (opp == '*')
+            {
+                Console.WriteLine("The answer is " + (num1 * num2));
+            }
+            else if (opp == '/')
+            {
+                Console.WriteLine("The answer is " + (num1 / num2));
+            }
+            else
+            {
+                Console.WriteLine("invalid");
+            }
+        }
+    }
+}
