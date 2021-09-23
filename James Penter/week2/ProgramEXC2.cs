11b7efe22a723ea4bce24e12e62fefa3d48d96f6
JayPenter <90903580+JayPenter@users.noreply.github.com>
2021-09-23 15:08:51 +0100

    Add files via upload

---
+++ James Penter/week2/ProgramEXC2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace EXC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string fullName;//assigning fullname 
+            
+            Console.WriteLine("What is your full name?");//asking for user input 
+            fullName = Console.ReadLine();//storing users answer
+            Console.WriteLine("So your name is: " + fullName);//asking user name is correct
+            Console.WriteLine("How about another name?...");//exit prompt
+            fullName = Console.ReadLine();
+            Console.WriteLine("How old are you?");
+            int Age = Int32.Parse(Console.ReadLine());//conversion needed as ALL user input is string
+            Console.WriteLine("Your age is..." + Age);
+            Console.WriteLine("How far do you travel to work?");
+            float distance = float.Parse(Console.ReadLine());
+            Console.WriteLine("The distance you travel is..." + distance);
+            Console.WriteLine("Press any key to close!");
+
+
+
+
+
+
+
+        }
+    }
+}
