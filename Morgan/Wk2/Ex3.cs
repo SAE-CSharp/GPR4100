01b51698d2cd0e7c53ebff3f53e2e0404383d66c
MCarpenter42 <90903467+MCarpenter42@users.noreply.github.com>
2021-09-24 11:42:16 +0100

    Update Ex3.cs

--- Morgan/Wk2/Ex3.cs
+++ Morgan/Wk2/Ex3.cs
@@ -13,11 +13,15 @@ namespace MCarpenter_Wk2_Ex3
             sub = x - y;
             mlt = x * y;
             mod = x % y;
-            
+
             Console.WriteLine("Addition:\nWhen x is " + x + " and y is " + y + ", the result of x + y is " + add + ".\n");
             Console.WriteLine("Subtraction:\nWhen x is " + x + " and y is " + y + ", the result of x - y is " + sub + ".\n");
             Console.WriteLine("Multiplication:\nWhen x is " + x + " and y is " + y + ", the result of x * y is " + mlt + ".\n");
-            Console.WriteLine("Modulus:\nWhen x is " + x + " and y is " + y + ", the modulus of x and y is " + mod + ".\n");
+            Console.WriteLine("Modulo:\nWhen x is " + x + " and y is " + y + ", the modulo of x and y is " + mod + ".\n");
+
+            float div = (float)x / y;
+
+            Console.WriteLine("Division:\nWhen x is " + x + " and y is " + y + ", the result of x / y is " + div + ".\n");
         }
     }
 }
