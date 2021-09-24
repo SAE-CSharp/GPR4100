4d7b807a6d56a9afe382880483fcb111bb055dc4
Seb Nicolas <90903412+snicolas3@users.noreply.github.com>
2021-09-24 16:45:05 +0100

    Create Exercise 1.cs

---
+++ Seb Nicolas/Lesson 1/Exercise 1.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+/*        static void Main(string[] args)
+        {
+            int Age = 18; // Assign Age Variable as Intager + Declare Age Variable Value
+            string Name = "Seb"; // Assign Name Variable as String + Declare Name Variable Value
+
+            int v1 = 10; // Assign Intager for v1
+            int v2 = 20; // Assign Intager for v2
+            int sum = v1 + v2; // Assign Intager for sum as v1+v2 making sum the sum of both variables.
+
+            Console.WriteLine(Name + " " + Age); // Name Variable + Space + Age Variable as a Console.WriteLine String
+            Console.WriteLine("Games Programming"); // Console.WriteLine String
+            Console.WriteLine(v1 + " + " + v2 + " = " + sum); // Console.WriteLine Simple Addition of Variables
+        }*/
+
+        static void Main(string[] args)
+        {
+            string nameOfStudent = "Jack";
+            int ageOfStudent = 35;
+
+            Console.WriteLine("This student's name is " + nameOfStudent);
+            Console.WriteLine("He is " + ageOfStudent + " years old.");
+            Console.WriteLine("He really likes the name " + nameOfStudent);
+            Console.WriteLine("But doesn't like being " + ageOfStudent);
+        }
+    }
+}
