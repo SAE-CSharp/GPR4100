5532efabb4a4fdd349563cee33d3e3e555fd7f6d
MCarpenter42 <90903467+MCarpenter42@users.noreply.github.com>
2021-09-24 15:56:40 +0100

    Update Ex6.cs

--- Morgan/Wk2/Ex6.cs
+++ Morgan/Wk2/Ex6.cs
@@ -19,7 +19,52 @@ namespace MCarpenter_Wk2_Ex6
             }
             else
             {
-                Console.WriteLine("\n" + intInputA + " is not divisible by " + intInputB);
+                Console.WriteLine("\n" + intInputA + " is not divisible by " + intInputB + "\n");
+            }
+
+            int intLarger = 0;
+            if (intInputA > intInputB)
+            {
+                Console.WriteLine(intInputA + " is the larger integer.");
+                intLarger = intInputA;
+            }
+            else if (intInputA < intInputB)
+            {
+                Console.WriteLine(intInputB + " is the larger integer");
+                intLarger = intInputB;
+            }
+            else
+            {
+                Console.WriteLine("The two integers are equal");
+            }
+
+            if (intLarger > 50)
+            {
+                Console.WriteLine("The larger integer is greater than 50");
+            }
+            else if (intLarger > 40)
+            {
+                Console.WriteLine("The larger integer is greater than 40");
+            }
+            else if (intLarger > 30)
+            {
+                Console.WriteLine("The larger integer is greater than 30");
+            }
+            else if (intLarger > 20)
+            {
+                Console.WriteLine("The larger integer is greater than 20");
+            }
+            else if (intLarger > 10)
+            {
+                Console.WriteLine("The larger integer is greater than 10");
+            }
+            else if (intLarger > 0)
+            {
+                Console.WriteLine("The larger integer is greater than 0");
+            }
+            else
+            {
+                Console.WriteLine("Neither integer is greater than 0");
             }
         }
     }
