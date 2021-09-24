cff4a553d16102cb9287a172e34538538c324338
Seb Nicolas <90903412+snicolas3@users.noreply.github.com>
2021-09-24 11:05:16 +0100

    Exercise 2

---
+++ Seb Nicolas.cs
@@ -0,0 +1,13 @@
+static void Main(string[] args)
+{
+    string myName = "Seb";
+    string myInstitute = "SAE London";
+    int myAge = 18;
+    string myTown = "Burgess Hill";
+    int instituteMiles = 68;
+
+    Console.WriteLine("My Name Is " + myName);
+    Console.WriteLine("I'm a student at " + myInstitute + ". I am " + myAge + " years old.");
+    Console.WriteLine("I Live in " + myTown + " which is " + instituteMiles + " miles away from " + myInstitute);
+    Console.WriteLine("My Hobbies are; Playing the Piano, Playing Video Games, and Coding");
+}
