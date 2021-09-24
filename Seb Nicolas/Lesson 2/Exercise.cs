304bd1cb2c922898e4f4354b96725efe327d1efb
Seb Nicolas <90903412+snicolas3@users.noreply.github.com>
2021-09-24 11:31:50 +0100

    Update

--- Seb Nicolas/Lesson 2/Exercise.cs
+++ Seb Nicolas/Lesson 2/Exercise.cs
@@ -1,13 +1,37 @@
-static void Main(string[] args)
 {
-    string myName = "Seb";
-    string myInstitute = "SAE London";
-    int myAge = 18;
-    string myTown = "Burgess Hill";
-    int instituteMiles = 68;
+    class Program
+    {
+        static void Main(string[] args)
+        {
 
-    Console.WriteLine("My Name Is " + myName);
-    Console.WriteLine("I'm a student at " + myInstitute + ". I am " + myAge + " years old.");
-    Console.WriteLine("I Live in " + myTown + " which is " + instituteMiles + " miles away from " + myInstitute);
-    Console.WriteLine("My Hobbies are; Playing the Piano, Playing Video Games, and Coding");
+            /* \' \" \\ \n \t */
+
+            string myName = "Seb";
+            string myInstitute = "SAE London";
+            int myAge = 18;
+            string myTown = "Burgess Hill";
+            int instituteMiles = 68;
+
+            Console.WriteLine("My Name Is " + myName);
+            Console.WriteLine("I'm a student at " + myInstitute + ". I am " + myAge + " years old.");
+            Console.WriteLine("I Live in " + myTown + " which is " + instituteMiles + " miles away from " + myInstitute);
+            Console.WriteLine("My Hobbies are; Playing the Piano, Playing Video Games, and Coding");
+            Console.WriteLine("My Folder Location is: C:\\Users\\Admin\\Exercise2.cs");
+
+            int x = 50, y = 20;
+            int result = x + y;
+            // + Addition
+            Console.WriteLine("\n The Result of X + Y Operation is " + (x + y));
+            // - Subtration
+            Console.WriteLine("\n The Result of X - Y Operation is " + (x - y));
+            // / Devision
+            Console.WriteLine("\n The Result of X / Y Operation is " + (x / y));
+            // * Multiply
+            Console.WriteLine("\n The Result of X * Y Operation is " + (x * y));
+            // % Modulus
+            Console.WriteLine("\n The Result of X % Y Operation is " + (x % y));
+
+
+        }
+    }
 }
