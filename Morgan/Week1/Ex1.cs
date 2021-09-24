8416f2c87fcec198e230c89900762d48573bc3ac
MCarpenter42 <90903467+MCarpenter42@users.noreply.github.com>
2021-09-24 11:24:43 +0100

    Update Ex1.cs

--- Morgan/Week1/Ex1.cs
+++ Morgan/Week1/Ex1.cs
@@ -7,18 +7,21 @@ namespace ConsoleAppStory_MCarpenter
         static void Main(string[] args)
         {
             string studentName = "Morgan";
+            /*char ch = 'Z'; // Use single quotes with character, double quotes with string*/
+            string firstChar = studentName.Substring(0, 1);
             int studentAge = 21;
             string studentUni = "SAE London";
             string studentLocation = "Bicester";
             float studentDistance = 52.3f;
             
-            Console.WriteLine("My name is "+studentName+"."); // Name
+            Console.WriteLine("My name is "+studentName+". The first letter of this name is "+firstChar+"."); // Name
             Console.WriteLine("I\'m "+studentAge+" years old, and I am a student at "+studentUni+"."); // Age + University
             Console.WriteLine("I live in "+studentLocation+",\nwhich is "+studentDistance+" miles away from my university.\n"); // Location + Distance to uni
 
             string filePath = "C:\\Users\\Admin\\Documents";
 
             Console.WriteLine("Example filepath: "+filePath);
+
         }
     }
 }
