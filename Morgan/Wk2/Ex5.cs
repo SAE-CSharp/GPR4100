792eb01d7b32cfffb74abf075f797791715d41ce
MCarpenter42 <90903467+MCarpenter42@users.noreply.github.com>
2021-09-24 14:38:50 +0100

    Update Ex5.cs

--- Morgan/Wk2/Ex5.cs
+++ Morgan/Wk2/Ex5.cs
@@ -6,22 +6,38 @@ namespace MCarpenter_Wk2_Ex5
     {
         static void Main(string[] args)
         {
-            string userName, confirmName;
+            string userName, cnfName, cnfAge;
+            int userAge;
             userName = "";
-            confirmName = "n";
+            cnfName = "n";
+            userAge = 0;
+            cnfAge = "n";
 
-            while (confirmName == "n")  {
+            while (cnfName == "n") {
 
                 Console.WriteLine("Please enter your name:");
                 userName = Console.ReadLine();
 
-                Console.WriteLine("\nYou have entered: " + userName);
-                Console.WriteLine("Is this your name? (y/n)");
-                confirmName = Console.ReadLine();
+                Console.WriteLine("\nYou have entered: " + userName + ". Is this your name? (y/n)");
+                cnfName = Console.ReadLine();
 
+                Console.WriteLine("");
             }
 
-            Console.WriteLine("Welcome, " + userName);
+            Console.WriteLine("Welcome, " + userName + "!\n");
+
+            while (cnfAge == "n") {
+
+                Console.WriteLine("Please enter your age:");
+                userAge = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine("\nYou have entered: " + userAge + ". Is this your age? (y/n)");
+                cnfAge = Console.ReadLine();
+
+                Console.WriteLine("");
+            }
+
+            Console.WriteLine(userName + " is " + userAge + " years old.");
         }
     }
 }
