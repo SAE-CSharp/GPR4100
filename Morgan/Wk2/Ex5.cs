c9914b288667796a889915e7651aa90bc5102008
MCarpenter42 <90903467+MCarpenter42@users.noreply.github.com>
2021-09-24 15:06:16 +0100

    Update Ex5.cs

--- Morgan/Wk2/Ex5.cs
+++ Morgan/Wk2/Ex5.cs
@@ -6,38 +6,77 @@ namespace MCarpenter_Wk2_Ex5
     {
         static void Main(string[] args)
         {
-            string userName, cnfName, cnfAge;
-            int userAge;
+            string userName, userUni, confirmYN;
             userName = "";
-            cnfName = "n";
-            userAge = 0;
-            cnfAge = "n";
+            userUni = "";
+            confirmYN = "n";
+            int userAge = 0;
+            float userDist = 0.0f;
 
-            while (cnfName == "n") {
+            while (confirmYN == "n") {
 
                 Console.WriteLine("Please enter your name:");
                 userName = Console.ReadLine();
 
                 Console.WriteLine("\nYou have entered: " + userName + ". Is this your name? (y/n)");
-                cnfName = Console.ReadLine();
+                confirmYN = Console.ReadLine();
 
                 Console.WriteLine("");
-            }
+            };
+            confirmYN = "n";
 
-            Console.WriteLine("Welcome, " + userName + "!\n");
+            /*Console.WriteLine("Welcome, " + userName + "!\n");*/
 
-            while (cnfAge == "n") {
+            while (confirmYN == "n") {
 
                 Console.WriteLine("Please enter your age:");
                 userAge = Int32.Parse(Console.ReadLine());
 
                 Console.WriteLine("\nYou have entered: " + userAge + ". Is this your age? (y/n)");
-                cnfAge = Console.ReadLine();
+                confirmYN = Console.ReadLine();
 
                 Console.WriteLine("");
-            }
+            };
+            confirmYN = "n";
 
-            Console.WriteLine(userName + " is " + userAge + " years old.");
+            /*Console.WriteLine(userName + " is " + userAge + " years old.");*/
+
+            while (confirmYN == "n")
+            {
+
+                Console.WriteLine("Please enter the university you are attending:");
+                userUni = Console.ReadLine();
+
+                Console.WriteLine("\nYou have entered: " + userUni + ". Is this correct? (y/n)");
+                confirmYN = Console.ReadLine();
+
+                Console.WriteLine("");
+            };
+            confirmYN = "n";
+
+            while (confirmYN == "n")
+            {
+
+                Console.WriteLine("Please enter the distance you live from your university, in miles:");
+                userDist = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("\nYou have entered: " + userDist + ". Is this correct? (y/n)");
+                confirmYN = Console.ReadLine();
+
+                Console.WriteLine("");
+            };
+            confirmYN = "n";
+
+            Console.WriteLine("-- USER INFORMATION --\n");
+            Console.WriteLine("Name:\n  " + userName + "\n");
+            Console.WriteLine("Age:\n  " + userAge + "\n");
+            Console.WriteLine("University:\n  " + userUni + "\n");
+            Console.WriteLine("Distance from home to uni:\n  " + userDist + " miles\n");
+
+            Console.WriteLine("-- MISC INFORMATION --\n");
+            
+            Char firstLetter = Char.Parse(userName.Substring(0, 1));
+            Console.WriteLine("First letter of your name:\n  " + firstLetter);
         }
     }
 }
