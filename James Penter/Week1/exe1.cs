7315053c73a28d01f3112bc263cd9ac2535c7058
JayPenter <90903580+JayPenter@users.noreply.github.com>
2021-09-30 15:45:55 +0100

    Create exe1.cs

---
+++ James Penter/Week1/exe1.cs
@@ -0,0 +1,41 @@
+ Console.WriteLine("Hello World!");
+            Console.WriteLine("Practice makes perfect!");
+            string Name = "John";
+            //Declaring name variable
+            int Zack = 25;
+            Console.WriteLine("I am " + Zack + " years old");
+            //Declaring integer variable can only be whole numbers
+            //float is 32 bit (Double is 64 bit)
+            int Age = 22;
+            int Time = 30;
+
+            Console.WriteLine("My name is James\n " + "it\'ll take me " + Time + " minutes to get from my home to work\n I get on the Liverpool train\\ Northern line\\ at Orrell Park station");
+            // \n=newline \'=adding apostrophe \\subsection or folder layout
+            //math prompts (/\*\-\+\)
+            //IF statments 
+
+            int result;
+            int x = 10;
+            int y = 20;
+            result = (x + y);
+            Console.WriteLine("x + y is " + result);
+            result = (x - y); // result value can be changed without being declared agfain it has already been declared before, just not given a value
+            Console.WriteLine("X minus Y is " + result);
+            result = (x * y);
+            Console.WriteLine("x * y is " + result);
+            result = (x / y);
+            Console.WriteLine("X/Y is " + result);
+            result = (x % y);
+            Console.WriteLine("X mod Y is " + result);
+            x--; // decrementing the value of x by one, must be done seperately as it is affecting the value of a variable
+            y++; // incrimenting the value of y by one
+            result = (x + y);
+            Console.WriteLine("(x-1) + (y+1) is " + result);
+
+            //*****************************************************************
+            int w = 7;
+            int p = 4;
+            Console.WriteLine("the result of int/int is: " + w / p);
+            Console.WriteLine("The result of float/int is: " + (float)w / p);
+            Console.WriteLine("The resuly of int/float is: " + w / (float)p);
+            Console.WriteLine("The result of float/float is: " + (float)w / (float)p);
