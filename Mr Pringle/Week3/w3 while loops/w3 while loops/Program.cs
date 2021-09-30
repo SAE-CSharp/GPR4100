3371f0b19ecace61df4cecf2d4822cc084f9e095
MrPringleGames <90903487+MrPringleGames@users.noreply.github.com>
2021-09-30 16:49:28 +0100

    Add files via upload

---
+++ Mr Pringle/Week3/w3 while loops/w3 while loops/Program.cs
@@ -0,0 +1,59 @@
+﻿
+using System;
+
+namespace w3_while_loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 20;
+            while (count > 10)
+            {
+                count++;
+                Console.WriteLine("Count is: " + count);
+                if (count > 40)
+                {
+                    break;
+                }
+
+                
+            }
+
+            while (count > 0)
+            {
+                count--;
+                
+                if (count == 2)
+                {
+                    continue;
+                }
+
+                Console.WriteLine("Count is: " + count);
+
+                if (count == 0)
+                {
+                    break;
+                }
+            }
+
+
+
+
+            char again = 'Y';
+
+            while (again == 'Y')
+            {
+                Console.WriteLine("\n**Played an exciting game**\n" +
+                    "Do you want to play again? Y/N");
+                again = Convert.ToChar(Console.ReadLine());
+
+
+
+            }
+            Console.WriteLine("\nOkay, Bye.");
+
+
+        }
+    }
+}
