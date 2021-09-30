494839b8dad36f05b8715d76daf34f7f1d81e773
MrPringleGames <90903487+MrPringleGames@users.noreply.github.com>
2021-09-30 14:49:05 +0100

    Add files via upload

---
+++ Mr Pringle/Week3/w3 game difficulty/w3 game difficulty/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace w3_game_difficulty
+{
+    class Program
+    {
+        enum difficulty { Easy='A', Medium='B', Hard='C', Insane='D' };
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your game difficulty!\nA. Easy\nB. Medium\nC. Hard\nD. Insane");
+            //int diffChoice = Convert.ToInt32(Console.ReadLine());
+            //string diffChoice = Console.ReadLine();
+            char diffChoice = Convert.ToChar(Console.ReadLine());
+
+
+            switch (diffChoice)
+            {
+                case (char)difficulty.Easy:
+                    Console.WriteLine("You've chosen the difficulty Easy");
+                break;
+                case (char)difficulty.Medium:
+                    Console.WriteLine("You've chosen the difficulty Medium");
+                break;
+                case (char)difficulty.Hard:
+                    Console.WriteLine("You've chosen the difficulty Hard");
+                break;
+                case (char)difficulty.Insane:
+                    Console.WriteLine("You're completely insane to choose this!\n\t\tWHY????");
+                break;
+                default:
+                    Console.WriteLine("Unkown difficulty chosen, please turn back and try again.");
+                break;
+            }
+        }
+    }
+}
---
+++ Mr Pringle/Week3/w3 switch/w3 switch/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace w3_switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter colour");
+            string colour = Console.ReadLine();
+            switch (colour)
+            {
+                case "black":
+                    Console.WriteLine("black colour");
+                    break;
+                case "red":
+                    Console.WriteLine("red colour");
+                    break;
+                case "green":
+                    Console.WriteLine("green colour");
+                    break;
+                default:
+                    Console.WriteLine("Unknown Colour");
+                    break;
+            }
+
+
+        }
+    }
+}
