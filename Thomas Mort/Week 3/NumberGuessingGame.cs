a57e7ba14abeee7ec53a4106ff52000f3d036912
ThomasMORTsae <90903480+ThomasMORTsae@users.noreply.github.com>
2021-10-04 20:46:58 +0100

    Update NumberGuessingGame.cs

--- Thomas Mort/Week 3/NumberGuessingGame.cs
+++ Thomas Mort/Week 3/NumberGuessingGame.cs
@@ -14,65 +14,102 @@ namespace Number_Guessing_Game
             NumberAnswer = randm.Next(1, 100);
 
             Console.WriteLine("What is your first guess between 1-100?");
+            Console.WriteLine(NumberAnswer);
             Guess1 = Int32.Parse(Console.ReadLine());
-            if (Guess1 == NumberAnswer)
-            {
-                Console.WriteLine("Well done! You got it on the first go!");
-            }
-            else if (Guess1 > NumberAnswer)
-            {
-                Console.WriteLine("Your answer of " + Guess1 + " Is higher than the answer! Try again!");
-                Guess2 = Int32.Parse(Console.ReadLine());
-                if (Guess2 == NumberAnswer)
+                if (Guess1 == NumberAnswer)
                 {
-                    Console.WriteLine("Your answer is right! Well done!");
+                    Console.WriteLine("Well done! You got it on the first go!");
                 }
-                else if (Guess2 > NumberAnswer)
+                else if (Guess1 > NumberAnswer)
                 {
-                    Console.WriteLine("Your answer of " + Guess2 + " Is higher than the answer again.. Try again though!");
-                    Guess3 = Int32.Parse(Console.ReadLine());
-                    if (Guess3 == NumberAnswer)
+                    Console.WriteLine("Your answer of " + Guess1 + " Is higher than the answer! Try again!");
+                    Guess2 = Int32.Parse(Console.ReadLine());
+                    if (Guess2 == NumberAnswer)
                     {
-                        Console.WriteLine("This is right! Well done!");
+                        Console.WriteLine("Your answer is right! Well done!");
                     }
-                    else if (Guess3 > NumberAnswer)
+                    else if (Guess2 > NumberAnswer)
                     {
-                        Console.WriteLine("This is still too high, im sorry..");
+                        Console.WriteLine("Your answer of " + Guess2 + " Is higher than the answer again.. Try again though!");
+                        Guess3 = Int32.Parse(Console.ReadLine());
+                        if (Guess3 == NumberAnswer)
+                        {
+                            Console.WriteLine("This is right! Well done!");
+                        }
+                        else if (Guess3 > NumberAnswer)
+                        {
+                            Console.WriteLine("This is still too high, im sorry..");
+                        }
+                        else if (Guess3 < NumberAnswer)
+                        {
+                            Console.WriteLine("This answer was too low.. Sorry!");
+                            Console.WriteLine("\n The real answer was" + NumberAnswer);
+                        }
                     }
-                    else if (Guess3 < NumberAnswer)
+                    else if (Guess2 < NumberAnswer)
                     {
-                        Console.WriteLine("This answer was too low.. Sorry!");
-                        Console.WriteLine("\n The real answer was" + NumberAnswer);
+                        Console.WriteLine("This answer is too low! Go higher!");
+                        Guess3 = Int32.Parse(Console.ReadLine());
+                        if (Guess3 == NumberAnswer)
+                        {
+                            Console.WriteLine("This is right! Well done!");
+                        }
+                        else if (Guess3 > NumberAnswer)
+                        {
+                            Console.WriteLine("This is still too high, im sorry..");
+                        }
+                        else if (Guess3 < NumberAnswer)
+                        {
+                            Console.WriteLine("This answer was too low.. Sorry!");
+                            Console.WriteLine("\n The real answer was" + NumberAnswer);
+                        }
                     }
                 }
-            }
-            else if (Guess1 < NumberAnswer)
-            {
-                Console.WriteLine("Your answer of " + Guess1 + " Is lower than the answer! Try again!");
-                Guess2 = Int32.Parse(Console.ReadLine());
-                if (Guess2 == NumberAnswer)
+                else if (Guess1 < NumberAnswer)
                 {
-                    Console.WriteLine("Your answer is right! Well done!");
-                }
-                else if (Guess2 > NumberAnswer)
-                {
-                    Console.WriteLine("Your answer of " + Guess2 + " Is higher than the answer again.. Try again though!");
-                    Guess3 = Int32.Parse(Console.ReadLine());
-                    if (Guess3 == NumberAnswer)
+                    Console.WriteLine("Your answer of " + Guess1 + " Is lower than the answer! Try again!");
+                    Guess2 = Int32.Parse(Console.ReadLine());
+                    if (Guess2 == NumberAnswer)
                     {
-                        Console.WriteLine("This is right! Well done!");
+                        Console.WriteLine("Your answer is right! Well done!");
                     }
-                    else if (Guess3 > NumberAnswer)
+                    else if (Guess2 > NumberAnswer)
                     {
-                        Console.WriteLine("This is still too high, im sorry..");
+                        Console.WriteLine("Your answer of " + Guess2 + " Is higher than the answer again.. Try again though!");
+                        Guess3 = Int32.Parse(Console.ReadLine());
+                        if (Guess3 == NumberAnswer)
+                        {
+                            Console.WriteLine("This is right! Well done!");
+                        }
+                        else if (Guess3 > NumberAnswer)
+                        {
+                            Console.WriteLine("This is still too high, im sorry..");
+                        }
+                        else if (Guess3 < NumberAnswer)
+                        {
+                            Console.WriteLine("This answer was too low.. Sorry!");
+                            Console.WriteLine("\n The real answer was" + NumberAnswer);
+                        }
                     }
-                    else if (Guess3 < NumberAnswer)
+                    else if (Guess2 < NumberAnswer)
                     {
-                        Console.WriteLine("This answer was too low.. Sorry!");
-                        Console.WriteLine("\n The real answer was" + NumberAnswer);
+                        Console.WriteLine("This answer is too low! Go higher!");
+                        Guess3 = Int32.Parse(Console.ReadLine());
+                        if (Guess3 == NumberAnswer)
+                        {
+                            Console.WriteLine("This is right! Well done!");
+                        }
+                        else if (Guess3 > NumberAnswer)
+                        {
+                            Console.WriteLine("This is still too high, im sorry..");
+                        }
+                        else if (Guess3 < NumberAnswer)
+                        {
+                            Console.WriteLine("This answer was too low.. Sorry!");
+                            Console.WriteLine("\n The real answer was" + NumberAnswer);
+                        }
                     }
                 }
             }
         }
     }
-}
