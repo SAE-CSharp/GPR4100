cdb1f7ac5bbe31406f47bebc20074f2a5ec3053d
Jfranco2000 <90903522+Jfranco2000@users.noreply.github.com>
2021-10-04 13:26:53 +0100

    Create GuessingGame.cs

---
+++ Josh/week3/GuessingGame.cs
@@ -0,0 +1,51 @@
+using System;
+
+namespace GuessingGame
+{
+    class Program                                       // REVISION NOTES: Research how to check the validity of the datatype of inputted data
+    {
+
+        static void Main(string[] args)
+        {
+            int randomNumber=6, userInput = 100, i = 0;
+
+            //Random randm = new Random();
+            //randomNumber = (Int32)(randm.Next(1, 11));
+
+            Console.WriteLine("You have 3 attempts to guess the chosen number between 1 and 10.\nWhat is your 1st guess?");
+
+            while (i < 3 && userInput != randomNumber)
+            {
+                i++;
+                userInput = Int32.Parse(Console.ReadLine());
+
+                if (userInput != randomNumber)
+                {
+                    Console.WriteLine("Your guess was incorrect!");
+
+                    if (i == 3)
+                    {
+                        Console.WriteLine("You are out of attempts.");
+                        break;
+                    }
+                    else if (userInput < randomNumber)
+                    {
+                        Console.Write("My number is higher than yours.\nTry again\n");
+                        continue;
+                    }
+                    else if (userInput > randomNumber)
+                    {
+                        Console.Write("My number is lower than yours.\nTry again\n");
+                        continue;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Congrats you guessed correctly!");
+                    break;
+                }
+            }
+            Console.WriteLine("The number was " + randomNumber);
+        }
+    }
+}
