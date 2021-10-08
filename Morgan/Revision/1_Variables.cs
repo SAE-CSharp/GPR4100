9cd00faca9c1e72ec8b7383c0927c688dacde8c5
MCarpenter42 <90903467+MCarpenter42@users.noreply.github.com>
2021-10-08 12:33:36 +0100

    Update 1_Variables.cs

--- Morgan/Revision/1_Variables.cs
+++ Morgan/Revision/1_Variables.cs
@@ -1,1 +1,21 @@
+using System;
 
+namespace MCarpenter_Revision1_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // A variable acts as a container for a single piece of data.
+            // Variables must be declared with a data type, and can then be assigned
+            // data of that type.
+            
+            int var_integer = 1;                    // Positive and negative whole numbers
+            string var_string = "Words go here";    // String of plaintext
+            char var_character = 'A';               // Single text character
+            float var_floatingPoint = 0.5f;         // 32-bit decimal number
+            double var_doublePrecision = 1.5;       // 64-bit decimal number
+            bool var_boolean = true;                // True/false
+        }
+    }
+}
