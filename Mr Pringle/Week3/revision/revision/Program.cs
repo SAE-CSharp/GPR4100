ad86d44c4d96778fee42b14e1fcc40d2df2cbc5a
MrPringleGames <90903487+MrPringleGames@users.noreply.github.com>
2021-10-11 15:44:24 +0100

    Add files via upload

---
+++ Mr Pringle/Week3/revision/revision/Program.cs
@@ -0,0 +1,223 @@
+﻿using System;
+
+namespace revision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Console.WriteLine("Enter name!");
+           // string name = Console.ReadLine();
+            //Console.WriteLine("Enter age!");
+            //int age = Convert.ToInt32(Console.ReadLine());
+            string name = "Dalton";
+            int age = 20;
+            Console.WriteLine("My Name is " + name + ",");
+            Console.WriteLine("and I am "+age+" Years Old.");
+            name = "Tom";                                               //the value of name has changed from this point forward unless changed again
+            age = 21;                                                   //the value of age has changed from this poinnt forward unless changed again
+            Console.WriteLine("I like the name "+name+",");
+            Console.WriteLine("but I don't like being "+age);
+            /////////////////////////////////////////////////
+            //datatypes
+            string campus = "Liverpool";                                //Liverpool i stoered in the variable of campuss
+            Console.WriteLine(campus.ToLower());                        // .ToLower makes everything lower case
+            Console.WriteLine(campus.ToUpper());                        // .ToUpper makes everything upper case
+            
+            Console.WriteLine("SAE "+campus);
+            Console.Write("Enter your campus name: ");
+            string campus1 = Console.ReadLine();
+            Console.WriteLine(campus1);
+            Console.WriteLine("60 + 40 in line = "+60+40);
+            int x = 60;
+            int y = 40;
+            int result = x + y;
+            Console.WriteLine("X + Y = "+ result);
+            /////////////////////////////////////////////////
+            //float
+            float speed = 5.6f;
+            //double
+            double speed1 = 5.6;
+            //decimal
+            decimal speed2 = 56;
+            //char
+            Console.WriteLine("Enter Grade!");
+            char grade = Convert.ToChar(Console.ReadLine());
+            //boolean
+            bool nought = true;
+            bool cross = false;
+            //increase and decrease
+            int a = 20;
+            
+
+            Console.WriteLine("Float "+speed);
+            Console.WriteLine("Double "+speed1);
+            Console.WriteLine("Decimal "+speed2);
+            Console.WriteLine("Char "+grade);
+            Console.WriteLine("Boolean "+nought);
+            Console.WriteLine("Boolean "+cross);
+            a = a++;
+            Console.WriteLine("Increase "+a);
+            a = a--;
+            Console.WriteLine("Decease "+a);
+            /////////////////////////////////////////////////
+
+            Console.WriteLine("Enter a number between 0 and 10!");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if(num > 10)                                                        //operant is num and 10, operator is >
+            {
+                Console.WriteLine("You're number should be 10 or less");
+            }
+            else if (num<0)
+            {
+                Console.WriteLine("Your number should be more than 0");
+            }
+            else
+            {
+                Console.WriteLine("Well Done");
+            }
+
+            //////////////////////////////////////////////////////
+            ///modulus operator %, it prints the remainder after dividing the first number by the second
+            Console.WriteLine("5 % 2 = "+ 5 % 2);
+            Console.WriteLine("6 % 3 = "+6 % 3);
+            Console.WriteLine("10 % 4 = "+10 % 4);
+            Console.WriteLine("3 % 2 = " + 3 % 2);
+            Console.WriteLine("11 % 5 = " + 11 % 5);
+            Console.WriteLine("17 & 4 = " + 17 % 4);
+          Console.WriteLine("Enter an Even or Odd number");
+            int eOo = Convert.ToInt32(Console.ReadLine());
+            if (eOo % 2 == 0)
+            {
+                Console.WriteLine("Tis Even");
+            }
+            else
+            {
+                Console.WriteLine("Tis Odd");
+            }
+
+            Console.WriteLine("Enter a multiple of 10 up to 30.");
+            int z = Convert.ToInt32(Console.ReadLine());
+            if (z == 10)
+            {
+                Console.WriteLine("Value of Z is 10");
+            }
+            else if (z == 20)
+            {
+                Console.WriteLine("Value of Z is 20");
+            }
+            else if (z == 30)
+            {
+                Console.WriteLine("Value of Z is 30");
+            }
+            else
+            {
+                Console.WriteLine("That wasn't an option.");
+            }
+
+            ///////////////////////////////////////////////////
+            Console.WriteLine("What day is it?");
+            string day = Console.ReadLine();
+            switch (day)
+            {
+                case "Monday":
+                {
+                    Console.WriteLine("Today is Monday");
+                    break;
+                }
+                case "Tuesday":
+                { 
+                    Console.WriteLine("Today is Tuesday");
+                    break;
+                }
+                case "Wednesday":
+                {
+                    Console.WriteLine("Today is Wednesday");
+                    break;
+                }
+                case "Thursday":
+                {
+                    Console.WriteLine("Today is Thrusday");
+                    break;
+                }
+                case "Friday":
+                {
+                    Console.WriteLine("Today is Friday");
+                    break;
+                }
+                case "Saturday":
+                {
+                    Console.WriteLine("Today is Saturday");
+                    break;
+                }
+                case "Sunday":
+                {
+                    Console.WriteLine("Today is Sunday");
+                    break;
+                }
+                default:
+                {
+                    Console.WriteLine("Invalid Use propper grammar and or spellting next time!");
+                    break;
+                }
+
+            }
+
+            switch (grade)
+            {
+                case 'A':
+                    Console.WriteLine("An A thats a Perfect Grade");
+                    break;
+                case 'B':                               //by doing this its a B OR C 
+                case 'C':
+                    Console.WriteLine("Enter your Grade again!");
+                    char option = Convert.ToChar(Console.ReadLine());
+                    if (option == 'B')
+                    {
+                        Console.WriteLine("B thats Brill");
+                    }
+                    if (option == 'C')
+                    {
+                        Console.WriteLine("C you just passed you should try harder");
+                    }
+                    break;
+                case 'D':
+                    Console.WriteLine("D? Yeah you failed");
+                    break;
+                case 'F':
+                    Console.WriteLine("F? thats worse than D");
+                    break;
+                default:
+                    Console.WriteLine("Invalid Grade");
+                    break;
+            }
+
+            ////////////////////////////////////////////////////////////////////////
+            
+            int tries = 3;
+            while(tries != 0 || tries < 0)
+            {
+                Console.WriteLine("Guess the number, its between 0 and 10.");
+                int guess = Convert.ToInt32(Console.ReadLine());
+                if (guess == num)
+                {
+                    Console.WriteLine("Congrats you guessed right!\nYou had "+tries+" left!");
+                    break;
+                }
+                else if (guess != num)
+                {
+                    tries--;
+                    Console.WriteLine("That's Incorrect, you have " + tries + " left!");
+                }
+
+            }
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Mr Pringle/Week3/w4 loops arrays and loop combinations/w4 loops arrays and loop combinations/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+
+namespace w4_loops_arrays_and_loop_combinations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //An Arrays is a collection of variables of the same type, reffered to by a common name. Arrays are stored in a sequential and ordered manner
+            // the first element of an array will have the index of 0
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };      //this array is 10 elaments long so its size is 10
+            // index numbers  0, 1, 2, 3, 4, 5, 6, 7, 8, 9      for the elements above
+            // the square brackets are the difference between a normal variable and an array
+            Console.Write(numbers[0] + " ");
+            Console.Write(numbers[1] + " ");
+            Console.Write(numbers[2] + " ");
+            Console.Write(numbers[3] + " ");
+            Console.Write(numbers[4] + " ");
+            Console.Write(numbers[5] + " ");
+            Console.Write(numbers[6] + " ");
+            Console.Write(numbers[7] + " ");
+            Console.Write(numbers[8] + " ");
+            Console.Write(numbers[9] + " ");
+            Console.WriteLine("\n");
+            Console.Write("Enter index number:");
+            int indexNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(numbers[indexNum]+" is the data stored in the index code entered");
+            ///////////////////////////////////////////////////
+            Console.WriteLine("\n");
+
+            string[] cities={ "Mumbai", "London", "New York", "Paris", "Dublin"};
+            Console.Write(cities[0] + " ");
+            Console.Write(cities[1] + " ");
+            Console.Write(cities[2] + " ");
+            Console.Write(cities[3] + " ");
+            Console.Write(cities[4] + " ");
+            Console.WriteLine("\n");
+            Console.Write("Enter City index number: ");
+            int indexCities = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(cities[indexCities] + " is the City you chose.");
+            /////////////////////////////////////////////////////
+            Console.WriteLine("\n");
+
+            string[] studentName = { "Dalton", "James", "Brandon", "Daisy", "Angela", "Tia", "Alex", "Aaron", "Josh", "Tommy", "Jack" };
+            Console.Write(studentName[0] + " ");
+            Console.Write(studentName[1] + " ");
+            Console.Write(studentName[2] + " ");
+            Console.Write(studentName[3] + " ");
+            Console.Write(studentName[4] + " ");
+            Console.Write(studentName[5] + " ");
+            Console.Write(studentName[6] + " ");
+            Console.Write(studentName[7] + " ");
+            Console.Write(studentName[8] + " ");
+            Console.Write(studentName[9] + " ");
+            Console.Write(studentName[10] + " ");
+            Console.WriteLine("\n");
+            Console.Write("Enter the Students Index: ");
+            int indexStudent = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Nice " + studentName[indexStudent] + " is definatly a student.");
+
+            int studentNameLength = studentName.Length;
+
+
+
+            float[] studentFloat = { 2.5f, 1.5f, 7.5f, 1.1f};
+            int floatLength = studentFloat.Length;
+            for (int i = 0; i < floatLength; i++)
+            {
+                Console.WriteLine("The index is: " + i + " and the element is: " + studentFloat[i]);
+            }
+            ///////////////////////////////////////////////////////
+            int[] book = new int[4];
+            //the numebr of elements of the array is 4
+            //the last index is 3 (0-3)
+            book[0] = 3;
+            book[1] = 6;
+            book[2] = 5;
+            book[3] = 10;
+            //this is decarling the elements of the array outside the array creation line
+            Console.WriteLine("\n");
+
+            int[] num = { 10, 20, 30, 40, 50, 60, 70, 80, 90 };
+            Console.Write(num[0] + " ");
+            Console.Write(num[1] + " ");
+            Console.Write(num[2] + " ");
+            Console.Write(num[3] + " ");
+            Console.Write(num[4] + " ");
+            Console.Write(num[5] + " ");
+            Console.Write(num[6] + " ");
+            Console.Write(num[7] + " ");
+            Console.Write(num[8] + " ");
+            Console.WriteLine("\n");
+            int arrayLenght = num.Length;
+            Console.WriteLine(arrayLenght+" lines in the array above detected!");
+            /*
+                        int[] number;
+                        number = new int[4];
+
+                        string[] word = new string[9];
+
+                        float[] deci;
+                        deci = new float[456];
+
+                        these are all arrays with the arrays elements to be definded later for the spesific array length thats defined here
+            */
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            ///FOR LOOPS
+            ///for loops are like whiles but have a counter built in for a set number of times of repeating
+            Console.WriteLine("\n");
+            int[] myNum = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+            int myNumLengh = myNum.Length;
+            for (int i = 0; i< myNumLengh ; i++)
+            {
+                Console.WriteLine("The Index is: " + i + " and the element is: " + myNum[i]);
+            }
+            for (int i = 0; i <= 10; i++) 
+            {
+                Console.WriteLine("Hello World! "+i);
+            }
+            //the i is a local variable to the for loop and will not leak out, thus no errors
+            
+
+            string[] campus = { "London", "Liverpool", "Oxford", "Glasgow" };
+            int campLen = campus.Length;
+            for (int i = 0; i <campLen; i++)
+            {
+                Console.WriteLine("The SAE Campus with the index number: " + i + " is the " + campus[i] + " campus");
+                if (i == 1)
+                {
+                    Console.WriteLine("Hello Liverpool!!!!");
+                    continue;
+                }
+            }
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
