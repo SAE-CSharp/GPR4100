2d30add8cdca6c6192288b695b24758b7960ceff
JurgitaJZ <90904053+JurgitaJZ@users.noreply.github.com>
2021-10-14 16:40:35 +0100

    Update methods1.cs

--- Angela/week5/methods1.cs
+++ Angela/week5/methods1.cs
@@ -7,11 +7,17 @@ namespace methods1
         //void means no data type
         static void Main(string[] args)
         {
+            
             Sayhello("James", 5);
             Sayhello("Tom", 6);
             Greeting();
             MyName("Jurgita", 30);
             GetImfo();
+            int res = AddNumbers(3, 5); //result
+            Console.WriteLine("the recieved number is " + res);
+            int result2 = AddNumbers2();
+            int result3 = result2 + 20; //not working
+            Console.WriteLine("The result is " + result2);
         }
 
         static void Sayhello(string name, int num)
@@ -37,6 +43,22 @@ namespace methods1
             Console.WriteLine("What is my age?");
             int myage = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Hello, my name is " + name2 + " and I am " + myage + " years old");
+        }
+        static int AddNumbers(int x, int y)
+        {
+            int result = x + y;
+            return result;
+        }
+
+        static int AddNumbers2()
+        {
+            Console.WriteLine("What is a value?");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What is b value?");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int result2 = a + b;
+            return result2;
+            
         }
     }
 }
