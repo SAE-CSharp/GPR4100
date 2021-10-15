a45667ca87080dd2356397515949e3330b91d8e2
Seb Nicolas <90903412+snicolas3@users.noreply.github.com>
2021-10-15 11:14:19 +0100

    Create HW Leaderboard.cs

---
+++ Seb Nicolas/Homework/HW Leaderboard.cs
@@ -0,0 +1,77 @@
+using System;
+
+namespace HW4
+{
+    class Program
+    {
+        enum player_names { Player1 = 1, Player2, Player3, Player4 };
+        static void Main(string[] args)
+        {
+            int arraylength = scores.Length;
+
+            Console.WriteLine("//====================Leaderboard======================//");
+            Console.WriteLine("//Player 1 ================================//");
+            Console.WriteLine("//Player 2 ================================//");
+            Console.WriteLine("//Player 3 ================================//");
+            Console.WriteLine("//Player 4 ================================//");
+
+            int first_playerscore;
+            int second_playerscore;
+            int third_playerscore;
+            int fourth_playerscore;
+
+            for (int i = 0; i < arraylength; i++)
+            {
+                if (i == 0)
+                {
+                    Console.WriteLine("Enter the first score!");
+                    first_playerscore = Convert.ToInt32(Console.ReadLine());
+                    scores[0] = first_playerscore;
+                    Console.WriteLine("|====================Leaderboard======================|");
+                    Console.WriteLine("| Player 1 ================================ " + scores[0]);
+                    Console.WriteLine("| Player 2 ================================ " + 0);
+                    Console.WriteLine("| Player 3 ================================ " + 0);
+                    Console.WriteLine("| Player 4 ================================ " + 0);
+                }
+                else if (i == 1)
+                {
+                    Console.WriteLine("Enter the second score!");
+                    second_playerscore = Convert.ToInt32(Console.ReadLine());
+                    scores[1] = second_playerscore;
+                    Console.WriteLine("|====================Leaderboard======================|");
+                    Console.WriteLine("| Player 1 ================================ " + scores[0]);
+                    Console.WriteLine("| Player 2 ================================ " + scores[1]);
+                    Console.WriteLine("| Player 3 ================================ " + 0);
+                    Console.WriteLine("| Player 4 ================================ " + 0);
+                }
+                else if (i == 2)
+                {
+                    Console.WriteLine("Enter the second score!");
+                    third_playerscore = Convert.ToInt32(Console.ReadLine());
+                    scores[2] = third_playerscore;
+                    Console.WriteLine("|====================Leaderboard======================|");
+                    Console.WriteLine("| Player 1 ================================ " + scores[0]);
+                    Console.WriteLine("| Player 2 ================================ " + scores[1]);
+                    Console.WriteLine("| Player 3 ================================ " + scores[2]);
+                    Console.WriteLine("| Player 4 ================================ " + 0);
+                }
+                else if (i == 3)
+                {
+                    Console.WriteLine("Enter the second score!");
+                    fourth_playerscore = Convert.ToInt32(Console.ReadLine());
+                    scores[3] = fourth_playerscore;
+                    Console.WriteLine("|====================Leaderboard======================|");
+                    Console.WriteLine("| Player 1 ================================ " + scores[0]);
+                    Console.WriteLine("| Player 2 ================================ " + scores[1]);
+                    Console.WriteLine("| Player 3 ================================ " + scores[2]);
+                    Console.WriteLine("| Player 4 ================================ " + scores[3]);
+
+                }
+                else
+                {
+                    Console.WriteLine("Scores Submitted.");
+                }
+
+            }
+
+        }
