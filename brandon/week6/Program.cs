3fe9fa2dc256a98c15604b7cfdf454bb056ebc04
UserTaken01 <90903609+UserTaken01@users.noreply.github.com>
2021-10-21 14:05:49 +0100

    Add files via upload

---
+++ brandon/week6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Formatted_outputs
+{
+    class Program
+    {
+        //-----Method Oveerloading------------
+
+        static void DrawInt(int x, int y)
+        {
+            //Draw integer
+            int ans = x + y;
+        }
+
+        static void DrawInt(double x, double y)
+        {
+            //Draw integer
+            double ans = x + y;
+        }
+        static void DrawInt(double x)
+        {
+            //Draw integer
+            double ans = x;
+        }
+
+        static void Main(string[] args)
+        {
+            //-----------------------------------------------------------
+            //formatted outputs
+            string str = "Hello World";
+            string str2 = "Hey world";
+            //standard print statement
+            Console.WriteLine(str);
+            //Formmatted string print statement
+            Console.Write("{0} {1}", str, str2);
+
+            string name = "Brandon"; 
+            int age = 18;
+            Console.WriteLine("My name is {0}, I am {1}", name, age);
+        }
+    }
+}
