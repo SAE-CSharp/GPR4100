48fef7c2f336061ccfecfbaa98ef0eb62d54ff0a
Chris <90903494+cmoussa4@users.noreply.github.com>
2021-10-22 16:27:49 +0100

    Create Animal.cs

---
+++ Chris/week6V2.cs/Animal.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    class Animal
+    {
+        protected string name;
+        protected int age;
+
+        public void Eat()
+        {
+            Console.WriteLine("the animal is eating...");
+        }
+    }
+}
