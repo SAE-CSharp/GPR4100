de94bb46c218a7983d6a8504cee27828e52a0a83
gsinghbassi <90903449+gsinghbassi@users.noreply.github.com>
2021-10-22 16:17:48 +0100

    Create Triangle.cs

---
+++ Garran/Week 6/inheritance/Triangle.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    class Triangle: Shape
+    {
+        string style;
+
+        public void setWidth(double Twidth)
+        {
+            this.width = Twidth;
+        }
+
+        public double getWidth()
+        {
+            return this.width;
+        }
+        public void setHeight(double Theight)
+        {
+            this.height = Theight;
+        }
+
+        public double getheight()
+        {
+            return this.height;
+        }
+
+        public double getArea()
+        {
+            return this.width * this.height / 2;
+        }
+    }
+}
