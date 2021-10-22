0ff821a43c7f90261db3010f81a7258f90f0d214
gsinghbassi <90903449+gsinghbassi@users.noreply.github.com>
2021-10-22 16:18:11 +0100

    Create Shape.cs

---
+++ Garran/Week 6/inheritance/Shape.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    class Shape
+    {
+        protected double width;
+        protected double height;
+
+
+    }
+}
