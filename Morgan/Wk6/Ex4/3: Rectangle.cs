feb0bfbdef9690ac240a914e09bf5eb69c8ce828
MCarpenter42 <90903467+MCarpenter42@users.noreply.github.com>
2021-10-22 16:16:16 +0100

    Create 3: Rectangle.cs

---
+++ Morgan/Wk6/Ex4/3: Rectangle.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MCarpenter_Wk6_Ex4
+{
+    class Rectangle : Shape
+    {
+        public double GetArea()
+        {
+            return this.width * this.height;
+        }
+    }
+}
