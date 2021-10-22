15c746ee151689d5837ee9e55bc1269db69a37fb
gsinghbassi <90903449+gsinghbassi@users.noreply.github.com>
2021-10-22 16:17:18 +0100

    Create Animal.cs

---
+++ Garran/Week 6/inheritance/Animal.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    class Animal
+    {
+        protected string name;
+        protected int age;
+
+        public void Eat()
+        {
+            Console.WriteLine("The animal is eating .....");
+        }
+
+    }
+}
