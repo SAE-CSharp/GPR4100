4da23120afd369444c9d2e9411f7b0327636dadd
SAE-CSharp <90071512+SAE-CSharp@users.noreply.github.com>
2021-10-24 15:26:32 +0100

    Create HM4.cs

---
+++ Homeworks/HM4.cs
@@ -0,0 +1,43 @@
+using System;
+
+namespace homework5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int counter = 4;
+            while (counter > 0)
+            {
+                // Entering the value of the variable n
+                Console.Write("Enter the value of variable n:  ");
+                int n = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+                // Printing the upper part of the triangle
+                for (int i = 1; i <= n; i++)
+                {
+                    PrintLine(1, i);
+                }
+                // Printing the bottom part of the triangle
+                // that is under the longest line
+                for (int line = n - 1; line >= 1; line--)
+                {
+                    PrintLine(1, line);
+                }
+
+                counter--;
+            }
+
+
+        }
+        static void PrintLine(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
